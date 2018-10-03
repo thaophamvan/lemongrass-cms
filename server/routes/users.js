b04a8d1994a7f570ad5693ebc3564ae4fef11ed7
thanh4890 <thanh4890@gmail.com>
2018-10-03 17:58:44 +0700

    Edit and delete

--- server/routes/users.js
+++ server/routes/users.js
@@ -14,4 +14,60 @@ router.get('/', (req, res) => {
   });
 })
 
+router.get('/new', (req, res) => {
+  res.render('users-new')
+})
+
+router.post('/new', (req, res) => {
+  var db = dbUtils.get()
+
+  var user = {
+    name: req.body.name,
+    description: req.body.description
+  }
+
+  db.create(user).then(user => {
+    res.redirect('/users');
+  }).catch(err => {
+    res.status(500).send({ error: err });
+  });
+})
+
+router.get('/:id/delete', (req, res) => {
+  var db = dbUtils.get()
+
+  db.delete(req.params.id).then(todo => {
+    res.redirect('/users')
+  }).catch(err => {
+    res.status(500).send({ error: err });
+  });
+})
+
+router.get('/:id/edit', (req, res) => {
+  var db = dbUtils.get()
+
+  db.read(req.params.id).then(user => {
+    res.render('users-edit', user);
+  }).catch(err => {
+    res.status(500).send({ error: err });
+  });
+})
+
+router.post('/:id/edit', (req, res) => {
+  var db = dbUtils.get()
+
+  var user = {
+    id: req.params.id,
+    name: req.body.name,
+    description: req.body.description,
+    _rev: req.body._rev
+  }
+
+  db.update(user.id, user).then(todo => {
+    res.redirect('/users')
+  }).catch(err => {
+    res.status(500).send({ error: err });
+  });
+})
+
 module.exports = router
