2b18aafe8a5a6fee0c29d99d0cb8f19020a06e94
thanh4890 <thanh4890@gmail.com>
2018-10-10 13:59:26 +0700

    Dummy element for routes

--- client/components/DrinkTemperature.js
+++ client/components/DrinkTemperature.js
@@ -1,7 +1,37 @@
 import React from 'react'
 
 const DrinkTemperature = () => (
-  <div>DrinkTemperature</div>
+  <div>
+    <h1>Drink Temperature</h1>
+
+    <a href="/drink-temperature/new" className="btn btn-primary btn-md">New</a>
+
+    <div className="table-responsive">
+      <table className="table table-striped table-sm">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Desired temperature</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>id</td>
+            <td>name</td>
+            <td>description</td>
+            <td>desired_temperature</td>
+            <td>
+              <a className="btn btn-secondary btn-sm" href="/drink-temperature/id/edit">Edit</a>
+              <a className="btn btn-danger btn-sm" href="/drink-temperature/id/delete">Delete</a>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 )
 
 export default DrinkTemperature
--- client/components/DrinkType.js
+++ client/components/DrinkType.js
@@ -1,7 +1,43 @@
 import React from 'react'
 
 const DrinkType = () => (
-  <div>DrinkType</div>
+  <div>
+    <h1>Drink Type</h1>
+
+    <a href="/drink-type/new" className="btn btn-primary btn-md">New</a>
+
+    <div className="table-responsive">
+      <table className="table table-striped table-sm">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Type</th>
+            <th>Description</th>
+            <th>Height</th>
+            <th>Diameter</th>
+            <th>Volume</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>id</td>
+            <td>name</td>
+            <td>type</td>
+            <td>description</td>
+            <td>height</td>
+            <td>diameter</td>
+            <td>volume</td>
+            <td>
+              <a className="btn btn-secondary btn-sm" href="/drink-type/id/edit">Edit</a>
+              <a className="btn btn-danger btn-sm" href="/drink-type/id/delete">Delete</a>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 )
 
 export default DrinkType
